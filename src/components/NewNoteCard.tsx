7d374407d51106826d7c6d8d0d52dba0e974fa8e
Emily Comin <emilycominn@gmail.com>
2024-02-09 21:53:08 -0300

    finalizando layout css

--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -1,5 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Trash, X } from "lucide-react";
+import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from "sonner";
 
